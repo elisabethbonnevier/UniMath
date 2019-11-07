8a3a0a88f7879d1d0cc8213327cc7bd77ed58400
Elisabeth Bonnevier <elbo@math.su.se>
2019-11-07 19:51:40 +0100

    Prove cube category is category.

--- UniMath/wip/cubical.v
+++ UniMath/wip/cubical.v
@@ -1,1 +1,50 @@
 (** Proof that the interval in cartesian cubical sets is tiny *)
+
+Require Import UniMath.CategoryTheory.Core.Categories.
+Require Import UniMath.Combinatorics.StandardFiniteSets.
+Require Import UniMath.Combinatorics.FiniteSets.
+Require Import UniMath.MoreFoundations.Propositions.
+
+(* Construction with natural numbers as objects *)
+
+Definition cube_precategory_ob_mor : precategory_ob_mor :=
+  make_precategory_ob_mor nat (λ (m n : nat), (stnset n) → (stnset m ⨿ stnset 2)).
+
+Definition cube_precategory_data : precategory_data.
+Proof.
+  exists cube_precategory_ob_mor.
+  split.
+  - intro n.
+    exact (λ (i : stnset n), inl i).
+  - intros l m n f g i.
+    induction (g i).
+    + exact (f a).
+    + exact (inr b).
+Defined.
+
+Definition cube_precategory : precategory.
+Proof.
+  exists cube_precategory_data.
+  use make_is_precategory_one_assoc.
+  - intros m n f.
+    apply funextfun.
+    intro i.
+    cbn.
+    now induction (f i).
+  - intros m n g.
+    apply idpath.
+  - intros k l m n f g h.
+    apply funextfun.
+    intro i.
+    cbn.
+    now induction (h i).
+Defined.
+
+Definition cube_category : category.
+Proof.
+  exists cube_precategory.
+  intros m n.
+  apply funspace_isaset.
+  apply isfinite_isaset.
+  apply isfinitecoprod; apply isfinitestn.
+Defined.
