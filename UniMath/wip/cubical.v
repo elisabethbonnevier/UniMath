10ab8922e0bdec4595f778594f4f589f67e43ce0
Elisabeth Bonnevier <elbo@math.su.se>
2019-11-08 17:49:15 +0100

    Try to construct binproduct

--- UniMath/wip/cubical.v
+++ UniMath/wip/cubical.v
@@ -4,8 +4,9 @@ Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.Combinatorics.StandardFiniteSets.
 Require Import UniMath.Combinatorics.FiniteSets.
 Require Import UniMath.MoreFoundations.Propositions.
+Require Import UniMath.CategoryTheory.limits.binproducts.
 
-(* Construction with natural numbers as objects *)
+(* Construction oc cartesian cube category using nat as object set *)
 
 Definition cube_precategory_ob_mor : precategory_ob_mor :=
   make_precategory_ob_mor nat (λ (m n : nat), (stnset n) → (stnset m ⨿ stnset 2)).
@@ -48,3 +49,32 @@ Proof.
   apply isfinite_isaset.
   apply isfinitecoprod; apply isfinitestn.
 Defined.
+
+Open Scope stn.
+
+Definition combined_fun {X : UU} (m n : nat) (f : stnset m → X) (g : stnset n → X) : stnset (m + n) → X.
+Proof.
+  intro a.
+  set (i := pr1 a).
+  set (b := natgtb m i).
+  apply bool_rect.
+  - set (make_stn m i b).
+
+Definition cube_category_binproduct : BinProducts cube_category.
+Proof.
+  intros m n.
+  use make_BinProduct.
+  - exact (m + n).
+  - exact (λ (i : stnset m), inl ((stn_left m n) i)).
+  - exact (λ (i : stnset n), inl ((stn_right m n) i)).
+  - use make_isBinProduct.
+    + exact (pr2 cube_category).
+    + intros l f g.
+      unfold iscontr.
+      use tpair.
+      * use tpair.
+        -- intro i.
+           set (j := pr1 i).
+           set (b := natgtb m j).
+           Check ● j.
+           exact (if b then f (j ,, b) else g (n j )).
